5903d4ef4fe87d18c83c0650812f4a128e0806da
baiqmaulaola-wq <baiqmaulaola@gmail.com>
2026-04-27 07:51:12 +0800

    Add files via upload

---
+++ materi3.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h3>1. Function tanpa parameter</h3>";
+function sapa() {
+    echo "Halo!";
+}
+
+sapa();
+
+echo "<h3>2. Function dengan parameter</h3>";
+function sapaNama($nama) {
+    echo "Halo, $nama!";
+}
+
+sapaNama("Maula");
+
+echo "<h3>3. Function dengan return</h3>";
+function tambah($a, $b) {
+    return $a + $b;
+}
+
+$hasil = tambah(5, 7);
+echo $hasil;
+
+?>
